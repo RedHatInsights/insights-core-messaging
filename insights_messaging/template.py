883c420cb8bb962c18f808007657c14d624622c5
Christopher Sams <csams@redhat.com>
2020-03-06 07:14:19 -0600

    Fix regex so it works with python3.6 and python3.7.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- insights_messaging/template.py
+++ insights_messaging/template.py
@@ -31,8 +31,8 @@ class DefaultingTemplate(Template):
     pattern = r"""
     \$(?:
       (?P<escaped>\$)                       |   # Escape sequence of two delimiters
-      (?P<named>(?a:[_a-z][_a-z0-9]*))      |   # delimiter and a Python identifier
-      {(?P<braced>(?a:[^}]*))}              |   # delimiter and a braced identifier
+      (?P<named>([_a-z][_a-z0-9]*))         |   # delimiter and a Python identifier
+      {(?P<braced>([^}]*))}                 |   # delimiter and a braced identifier
       (?P<invalid>)                             # Other ill-formed delimiter exprs
     )
     """
