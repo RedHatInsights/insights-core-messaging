75320502c5169a98f1a48c6950700270451cc0c0
Christopher Sams <csams@redhat.com>
2024-02-28 16:24:46 -0600

    bump version to 1.2.9
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ rabbitmq = set([
 if __name__ == "__main__":
     setup(
         name="insights-core-messaging",
-        version="1.2.8",
+        version="1.2.9",
         description="Messaging service around insights-core.",
         long_description=open("README.md").read(),
         long_description_content_type='text/markdown',
